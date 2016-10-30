6dc6cfc7273d3769baa5251fb1876f7af85bcfc5
Cristian Caucott <criss.acv@gmail.com>
2016-10-30 00:49:16 -0300

    Crear controlador para sobreescribir métodos de la gema devise + reestructurar carpetas de controladores y vista.

---
+++ app/controllers/my_devise/registrations_controller.rb
@@ -0,0 +1,17 @@
+class MyDevise::RegistrationsController < Devise::RegistrationsController
+
+	def new
+		byebug
+    super
+  end
+
+  def create
+  	byebug
+  	super
+  end
+
+	def signUp
+		byebug
+	end
+
+end

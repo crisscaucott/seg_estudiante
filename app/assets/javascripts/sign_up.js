97c1de96ccbc67c691fbf01a99373bf3f0781512
Cristian Caucott <criss.acv@gmail.com>
2016-11-03 13:20:04 -0300

    JS para el sign up de usuario.

---
+++ app/assets/javascripts/sign_up.js
@@ -0,0 +1,30 @@
+// Evento de enviar formulario
+$( "form#sign_up_user" ).submit(function( event ) {
+	// Se evita que se recargue la pagina al enviar el formulario
+	event.preventDefault();
+	var data;
+	data = $(event.target).serialize();
+
+	$.ajax({
+		url: event.target.action,
+		data: data,
+		method: event.target.method
+	}).done(function(data, textStatus, jqXHR) {
+			console.log("done");
+			console.log(data);
+			console.log(textStatus);
+			console.log(jqXHR);
+
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+			console.log("fail");
+			console.log(jqXHR);
+			console.log(textStatus);
+			console.log(errorThrown);
+  }).always(function(data, textStatus, errorThrown) {
+  		console.log("always");
+  		console.log(data);
+  		console.log(textStatus);
+  		console.log(errorThrown);
+  });
+
+});

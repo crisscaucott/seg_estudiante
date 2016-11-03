85943d13171876818b1d4049bc42bc5db97273b9
Cristian Caucott <criss.acv@gmail.com>
2016-11-03 18:45:48 -0300

    Login, sign up y log out funcionando.

--- app/controllers/my_devises/registrations_controller.rb
+++ app/controllers/my_devises/registrations_controller.rb
@@ -1,5 +1,5 @@
 class MyDevises::RegistrationsController < Devise::RegistrationsController
-
+	include ApplicationHelper
 	private :sign_up_params
 
 	def new
@@ -11,23 +11,31 @@ class MyDevises::RegistrationsController < Devise::RegistrationsController
     build_resource(sign_up_params)
 
     if resource.valid?
+    	# Campos de usuario validados.
 	    if resource.save
+	    	# Guardar el usuario en BD exitosamente.
 	      if resource.active_for_authentication?
 	        set_flash_message :notice, :signed_up if is_navigational_format?
 	        sign_up(resource_name, resource)
-	        return render :json => {:success => true}
+	        # return render :json => {:success => true}
+
 	      else
 	        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
 	        expire_session_data_after_sign_in!
-	        return render :json => {:success => true}
+	        # return render :json => {:success => true}
 	      end
+
+    		return render :js => windowLocation(root_path)
+
 	    else
+	    	# Fallo con guardar en la BD
 	      clean_up_passwords resource
-	      return render :json => {:success => false}
+	      return render :json => {errors: "Ha ocurrido un error en registrar el usuario ingresado."}, status: 422
 	    end
 
     else
     	# Usuario no valido, por sus campos.
+	    clean_up_passwords resource
     	return render json: {errors: resource.getFormatErrorMessages}, status: 422
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  devise_scope :user do
+  	root to: "main#index"
+  end
+
   # You can have the root of your site routed with "root"
-  root 'main#index'
+  # root 'main#index'
 
 end

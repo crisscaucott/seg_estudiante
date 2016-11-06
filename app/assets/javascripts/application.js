64a6a5f0bf464ba49ef01e383b8112f77ecb4502
Cristian Caucott <criss.acv@gmail.com>
2016-11-05 21:57:29 -0300

    Notificaciones usando la librería nueva JS funcionando.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,18 +17,59 @@
 //= require bootstrap-filestyle.min
 //= require bootstrap-notify.min
 
-function showNoty(options)
+var notifications = [];
+var notifications_limit = 3;
+
+function showNotification(options)
 {
-	noty({
-		layout: 'topRight',
-		theme: 'defaultTheme', // or 'relax'
-	  type: 'error',
-		text: options.text,
-		animation: {
-        open: 'animated fadeInRight',
-        close: 'animated fadeOutRight',
-        easing: 'swing',
-        speed: 250
-    },
+	var type, title, dismissable;
+	type = options.type === null ? 'info' : options.type;
+	title = options.title === null ? null : options.title;
+	dismissable = options.dismiss === null ? true : options.dismiss;
+
+	if (options.closeAll !== null && options.closeAll)
+	{
+		$.notifyClose();
+		notifications = [];
+	}
+
+	notif = $.notify({
+		// options
+		// icon: 'glyphicon glyphicon-warning-sign',
+		title: title,
+		message: options.msg,
+	},{
+		// settings
+		type: type,
+		allow_dismiss: dismissable,
+		newest_on_top: true,
+		placement: {
+			from: "top",
+			align: "right"
+		},
+		offset:{
+			y: 68,
+			x: 5
+		},
+		spacing: 10,
+		delay: 0,
+		timer: 1000,
+		animate: {
+			enter: 'animated fadeInRight',
+			exit: 'animated fadeOutRight'
+		},
+		icon_type: 'class',
 	});
+
+	checkNotifications(notif);
+}
+
+function checkNotifications(noti)
+{
+	if (notifications.length >= notifications_limit)
+	{
+		notifications[0].close();
+		notifications.shift();
+	}
+	notifications.push(notif);
 }

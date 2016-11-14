4cfdae9fd80f73116b311bd3d873959b2fe58edb
Cristian Caucott <criss.acv@gmail.com>
2016-11-14 00:40:01 -0300

    Loguear con éxito al usuario por ajax.

--- app/controllers/my_devises/sessions_controller.rb
+++ app/controllers/my_devises/sessions_controller.rb
@@ -11,10 +11,25 @@ class MyDevises::SessionsController < Devise::SessionsController
   end
 
   def create
-    super
-    # byebug
-    # user = User.find_for_database_authentication(email: params[:user][:email])
+    user = User.find_for_database_authentication(email: params[:user][:email])
+    
+    return invalid_login_attempt unless user
+
+    if user.valid_password?(params[:user][:password])
+      # Usuario logueado con exito
+      sign_in :user, user
+      return render :js => windowLocation(root_path)
+    else
+      invalid_login_attempt
+      
+    end
   end
+
+  protected
+    def invalid_login_attempt
+      set_flash_message(:alert, :invalid)
+      render json: flash[:alert], status: 401
+    end
 
   # def sign_in_params
   # 	params.require(:user).permit(:name, :last_name, :email, :password, :password_confirmation)

2d5201ec6fb53c8f63272bb4e0d81c6f46f38193
Cristian Caucott <criss.acv@gmail.com>
2016-11-14 01:20:10 -0300

    Crear modelo y migración de la tabla de reportes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103215500) do
+ActiveRecord::Schema.define(version: 20161114041247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reportes", force: :cascade do |t|
+    t.string   "nombre_reporte",                 null: false
+    t.string   "tipo_reporte",                   null: false
+    t.integer  "usuario_id",                     null: false
+    t.boolean  "descargado",     default: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
 
   create_table "user_permissions", force: :cascade do |t|
     t.string "name", null: false
@@ -41,5 +50,6 @@ ActiveRecord::Schema.define(version: 20161103215500) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "reportes", "users", column: "usuario_id"
   add_foreign_key "users", "user_permissions", column: "id_permission"
 end

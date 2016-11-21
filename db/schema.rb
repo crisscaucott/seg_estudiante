6f0c3f89bce52c848d7790a7b80416078a421011
Cristian Caucott <criss.acv@gmail.com>
2016-11-21 01:22:36 -0300

    Migración: crear tabla de asignaturas.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121041552) do
+ActiveRecord::Schema.define(version: 20161121041949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "asignatura", force: :cascade do |t|
+    t.string   "nombre",        null: false
+    t.string   "codigo",        null: false
+    t.integer  "creditos",      null: false
+    t.datetime "fecha_borrado"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "carrera", force: :cascade do |t|
     t.integer  "duracion_formal",   null: false

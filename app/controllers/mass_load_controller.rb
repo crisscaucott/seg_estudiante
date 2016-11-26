9795493bab55b782d8ce30490ec965281259fe17
Cristian Caucott <criss.acv@gmail.com>
2016-11-26 08:48:35 -0300

    Configuración base del controlador para que acepte archivos excel para la subida de asistencia.

--- app/controllers/mass_load_controller.rb
+++ app/controllers/mass_load_controller.rb
@@ -1,4 +1,6 @@
 class MassLoadController < ApplicationController
+	include MassLoadHelper
+
 	def notas
 		report = Reporte.new
 		render action: :index, locals: {partial: 'notas',  report: report}
@@ -15,4 +17,19 @@ class MassLoadController < ApplicationController
 
 	def index
 	end
+
+
+	def uploadAssistance
+		uploaded_file = uploadFile(params[:reporte][:file])
+		if !uploaded_file[:file_path].nil?
+			mass_load_obj = LogCargaMasiva.new(usuario_id: current_user.id)
+
+
+		else
+			# Problema con guardar el fichero
+			render json: {msg: "Ha ocurrido un problema en guardar el archivo."}, status: :unprocessable_entity
+		end
+
+	end
+
 end

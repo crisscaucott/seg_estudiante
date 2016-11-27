9076f5100a71e6576df4b5f0bfd6bc4a41e8cbae
Cristian Caucott <criss.acv@gmail.com>
2016-11-27 16:01:46 -0300

    Fix en mostrar las mensajes de errores en la vista de login.

--- app/assets/javascripts/sign_up.js
+++ app/assets/javascripts/sign_up.js
@@ -1,28 +1,37 @@
 // Evento de enviar formulario
 $( "form#sign_up_user" ).submit(function( event ) {
+	var data, btn;
 	// Se evita que se recargue la pagina al enviar el formulario
 	event.preventDefault();
-	var data;
 	data = $(event.target).serialize();
+	btn = $(event.target).find('input[type=submit]');
+	btn_text = btn.val();
 
 	// showNoty();
 	$.ajax({
 		url: event.target.action,
 		data: data,
-		method: event.target.method
+		method: event.target.method,
+		beforeSend: function()
+		{
+			btn.val('Registrando...');
+			btn.toggleClass('disabled');
+		}
 	}).done(function(data, textStatus, jqXHR) {
-			console.log("done");
-			console.log(data);
-			console.log(textStatus);
-			console.log(jqXHR);
+			// console.log("done");
+			// console.log(data);
+			// console.log(textStatus);
+			// console.log(jqXHR);
 
   }).fail(function(jqXHR, textStatus, errorThrown) {
-			console.log("fail");
-			showNoty({text: jqXHR.responseJSON.errors})
+			showNotification({msg: jqXHR.responseJSON.errors, type: 'danger'})
 			console.log(jqXHR);
 			console.log(textStatus);
 			console.log(errorThrown);
+
   }).always(function(data, textStatus, errorThrown) {
+  		btn.toggleClass('disabled');
+  		btn.val(btn_text);
   		console.log("always");
   		console.log(data);
   		console.log(textStatus);

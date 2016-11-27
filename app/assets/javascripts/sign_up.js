547cf775ef95560525cee6009dc10a90c9730142
Cristian Caucott <criss.acv@gmail.com>
2016-11-27 17:15:19 -0300

    Validar rut desde cliente en el formulario de creacion de usuario.

--- app/assets/javascripts/sign_up.js
+++ app/assets/javascripts/sign_up.js
@@ -4,38 +4,44 @@ $( "form#sign_up_user" ).submit(function( event ) {
 	// Se evita que se recargue la pagina al enviar el formulario
 	event.preventDefault();
 	data = $(event.target).serialize();
+	rut = $(event.target).find("input[name='user[rut]']").val()
 	btn = $(event.target).find('input[type=submit]');
 	btn_text = btn.val();
 
-	// showNoty();
-	$.ajax({
-		url: event.target.action,
-		data: data,
-		method: event.target.method,
-		beforeSend: function()
-		{
-			btn.val('Registrando...');
-			btn.toggleClass('disabled');
-		}
-	}).done(function(data, textStatus, jqXHR) {
-			// console.log("done");
-			// console.log(data);
-			// console.log(textStatus);
-			// console.log(jqXHR);
+	if (checkRut(rut)) 
+	{
+		$.ajax({
+			url: event.target.action,
+			data: data,
+			method: event.target.method,
+			beforeSend: function()
+			{
+				btn.val('Registrando...');
+				btn.toggleClass('disabled');
+			}
+		}).done(function(data, textStatus, jqXHR) {
+				// console.log("done");
+				// console.log(data);
+				// console.log(textStatus);
+				// console.log(jqXHR);
 
-  }).fail(function(jqXHR, textStatus, errorThrown) {
-			showNotification({msg: jqXHR.responseJSON.errors, type: 'danger'})
-			console.log(jqXHR);
-			console.log(textStatus);
-			console.log(errorThrown);
+	  }).fail(function(jqXHR, textStatus, errorThrown) {
+				showNotification({msg: jqXHR.responseJSON.errors, type: 'danger'})
+				console.log(jqXHR);
+				console.log(textStatus);
+				console.log(errorThrown);
 
-  }).always(function(data, textStatus, errorThrown) {
-  		btn.toggleClass('disabled');
-  		btn.val(btn_text);
-  		console.log("always");
-  		console.log(data);
-  		console.log(textStatus);
-  		console.log(errorThrown);
-  });
+	  }).always(function(data, textStatus, errorThrown) {
+	  		btn.toggleClass('disabled');
+	  		btn.val(btn_text);
+	  		console.log("always");
+	  		console.log(data);
+	  		console.log(textStatus);
+	  		console.log(errorThrown);
+	  });		
+	}else
+	{
+		showNotification({msg: 'El rut ingresado no es válido.', type: 'danger'})
+	}
 
 });

7b11956920b83a0f16fa6483aa5aa0b9926c99e2
Cristian Caucott <criss.acv@gmail.com>
2016-12-06 23:37:55 -0300

    Mandar la solicitud de recuperación de contraseña mediante AJAX y redireccionarlo a la página de login tras enviarlo exitosamente.

---
+++ app/assets/javascripts/recuperar_pass.js
@@ -0,0 +1,39 @@
+// Envio formulario con los filtros de las asistencias.
+$('form#new_user').submit(function(event){
+  var data, btn, btn_text, noti_params = {msg: null, type: 'info'};
+  event.preventDefault();
+  data = $(event.target).serialize();
+  btn = $(event.target).find('input[type=submit]');
+  btn_text = btn.val();
+
+  $.ajax({
+    url: event.target.action,
+    data: data,
+    method: event.target.method,
+    beforeSend: function()
+    {
+      btn.val('Enviando correo...');
+      btn.toggleClass('disabled');
+    }
+  }).done(function(data, textStatus, jqXHR) {
+      noti_params.msg = dat.msg;
+      noti_params.type = 'success';
+
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+
+      noti_params.msg = errorThrown;
+      noti_params.type = 'danger';
+
+      if (jqXHR.responseJSON !== undefined)
+      {
+        noti_params.msg = jqXHR.responseJSON.msg;
+        noti_params.type = jqXHR.responseJSON.type;
+      }
+
+  }).always(function(data, textStatus, errorThrown) {
+      btn.toggleClass('disabled');
+      btn.val(btn_text);
+      showNotification({msg: noti_params.msg, type: noti_params.type})
+  });
+
+});
---
+++ app/controllers/my_devises/passwords_controller.rb
@@ -0,0 +1,34 @@
+class MyDevises::PasswordsController < Devise::PasswordsController
+	include ApplicationHelper
+
+	def new
+		super
+	end
+
+	def create
+		self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      # respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+    
+    	# Esto es hermoso!
+    	flash[:msg] = "Se ha enviado el correo de recuperación de contraseña al email '#{resource.email}'".
+    	flash[:alert_type] = 'success'
+    	flash.keep(:msg)
+    	flash.keep(:alert_type)
+    	render js: windowLocation(new_session_path(resource_name))
+    else
+    	render json: {msg: "No se pudo enviar el correo de recuperación de contraseña con el email ingresado.", type: 'danger'}, status: :bad_request
+      # respond_with(resource)
+    end
+		
+	end
+
+	protected
+		# The path used after sending reset password instructions
+    def after_sending_reset_password_instructions_path_for(resource_name)
+      new_session_path(resource_name) if is_navigational_format?
+    end
+
+end

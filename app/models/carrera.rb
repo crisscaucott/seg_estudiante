01ceac4e0d72d3c488f00663b51eb65212891370
Cristian Caucott <criss.acv@gmail.com>
2016-12-13 20:57:20 -0300

    Configurar asociaciones entre los modelos de usuario, escuela y carrera.

--- app/models/carrera.rb
+++ app/models/carrera.rb
@@ -2,6 +2,7 @@ class Carrera < ActiveRecord::Base
 	self.table_name = 'carrera'
 	has_and_belongs_to_many :asignaturas
 	has_many :estudiantes, class_name: "Estudiante", foreign_key: "carrera_id"
+	belongs_to :escuela, class_name: "Escuela", foreign_key: :escuela_id
 
 	def self.getCarreras
 		return self.select([:id, :nombre]).order(nombre: :asc)
--- app/models/escuela.rb
+++ app/models/escuela.rb
@@ -1,2 +1,7 @@
 class Escuela < ActiveRecord::Base
+	self.table_name = 'escuela'
+	has_many :carreras, class_name: "Carrera"
+	belongs_to :director, class_name: "User", foreign_key: :director_id
+
+	validates_presence_of :nombre, :director_id
 end

9acc0d54adb6e3a0901836bd6810ba423a9f4c00
Cristian Caucott <criss.acv@gmail.com>
2016-12-14 22:10:41 -0300

    Reconfigurar asociaciones entre las talbas de usuario y escuela.

--- app/models/escuela.rb
+++ app/models/escuela.rb
@@ -1,7 +1,12 @@
 class Escuela < ActiveRecord::Base
 	self.table_name = 'escuela'
 	has_many :carreras, class_name: "Carrera"
-	belongs_to :director, class_name: "User", foreign_key: :director_id
+	has_many :directores, class_name: "User", foreign_key: :escuela_id
+	# belongs_to :director, class_name: "User", foreign_key: :director_id
 
-	validates_presence_of :nombre, :director_id
+	validates_presence_of :nombre
+
+	def self.getEscuelas
+		return self.select([:id, :nombre]).order(nombre: :asc).all
+	end
 end

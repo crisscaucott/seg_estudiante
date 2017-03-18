643d95ca05f17fb55cbf81d64d37310fcf9f5e59
Cristian Caucott <criss.acv@gmail.com>
2017-03-18 06:27:28 -0300

    Agregar campo de nombre forrajeado para las tablas de carrera y asignatura.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315081846) do
+ActiveRecord::Schema.define(version: 20170318090749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,12 +27,13 @@ ActiveRecord::Schema.define(version: 20170315081846) do
   end
 
   create_table "asignatura", force: :cascade do |t|
-    t.string   "nombre",        null: false
+    t.string   "nombre",            null: false
     t.string   "codigo"
     t.integer  "creditos"
     t.datetime "fecha_borrado"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "nombre_formateado", null: false
   end
 
   create_table "asignatura_carrera", force: :cascade do |t|
@@ -69,6 +70,7 @@ ActiveRecord::Schema.define(version: 20170315081846) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "escuela_id",        null: false
+    t.string   "nombre_formateado", null: false
   end
 
   create_table "destinos", force: :cascade do |t|

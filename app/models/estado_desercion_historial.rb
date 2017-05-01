3371a863329e53cc7308ef8c99df11b7e3ccdbb7
Cristian Caucott <criss.acv@gmail.com>
2017-05-01 16:37:17 -0300

    Configurar relaciones de los modelos para el historial de estados de desercion

--- app/models/estado_desercion_historial.rb
+++ app/models/estado_desercion_historial.rb
@@ -1,2 +1,6 @@
 class EstadoDesercionHistorial < ActiveRecord::Base
+	self.table_name = 'estado_desercion_historial'
+	belongs_to :estudiante, class_name: "Estudiante"
+	belongs_to :estado_desercion, class_name: "EstadoDesercion"
+	belongs_to :usuario, class_name: "User"
 end
